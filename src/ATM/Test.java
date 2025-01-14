916ed4cd1311b25b86311ae38e444c77b0d9da2b
con <contrue_ct@outlook.com>
2025-01-14 15:50:15 +0800

    输入完成按回车

--- src/ATM/Test.java
+++ src/ATM/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         ATM atm = new ATM();
         System.out.println("欢迎使用ATM取款机");
+        System.out.println("输入完成后请按回车键");
         atm.start();
     }
 }
