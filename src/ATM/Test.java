fab17a52bb06d99dc618475421bbd8a446202d03
con <contrue_ct@outlook.com>
2025-01-14 15:43:23 +0800

    第一次修改

--- src/ATM/Test.java
+++ src/ATM/Test.java
@@ -3,6 +3,7 @@ package ATM;
 public class Test {
     public static void main(String[] args) {
         ATM atm = new ATM();
+        System.out.println("欢迎使用ATM取款机");
         atm.start();
     }
 }
