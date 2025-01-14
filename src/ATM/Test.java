55097d2caeb4011d1393ca5874192163aa7e336b
con <contrue_ct@outlook.com>
2025-01-14 16:06:14 +0800

    输入一句话啊

--- src/ATM/Test.java
+++ src/ATM/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         ATM atm = new ATM();
         System.out.println("欢迎使用ATM取款机");
-        System.out.println("请输入一句话");
+        System.out.println("请输入一句话啊");
         atm.start();
     }
 }
