3d92dc4796b899bfd93bec0fb870711b2d6935b6
con <contrue_ct@outlook.com>
2025-01-14 15:48:10 +0800

    完成输入卡号

--- src/ATM/Test.java
+++ src/ATM/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         ATM atm = new ATM();
         System.out.println("欢迎使用ATM取款机");
+        System.out.println("请输入卡号：");
         atm.start();
     }
 }
