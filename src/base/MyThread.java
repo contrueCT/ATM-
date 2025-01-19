8b215ad07a4f6b68e95a19db194fd4b20819fbab
con <contrue_ct@outlook.com>
2025-01-20 00:21:02 +0800

    feat:尝试了多线程创建

---
+++ src/base/MyThread.java
@@ -0,0 +1,8 @@
+package base;
+
+public class MyThread extends Thread{
+    @Override
+    public void run() {
+        super.run();
+    }
+}
--- src/base/caogao.java
+++ src/base/caogao.java
@@ -5,19 +5,18 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class caogao {
-    public static void main(String[] args) {
-        Date d = new Date();
-        System.out.println(d);
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new MyThread());
+        Thread t2 = new Thread(new MyThread());
+        t1.start();
+        t2.start();
 
-        long time = d.getTime();
-        System.out.println(time);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss EEE a");
-        System.out.println(sdf.format(d));
-
-        String rs = sdf.format(d);
-        String rs2 = sdf.format(time);
-        System.out.println(rs);
-        System.out.println(rs2);
-        
+        System.out.println(t1.getName());
+        System.out.println(t2.getName());
+        Thread m = Thread.currentThread();
+        System.out.println(m.getName());
+        for(int i = 0;i<6;i++){
+            System.out.println("main: "+i);
+        }
     }
 }
